b9a84080893db221f23e3a716006c0d121a9d32b
Scott Rogener <srogener@gmail.com>
2024-03-17 21:21:24 -0400

    rename assets better

--- JvmConcurrencyFrontend/src/domains/scenarios.ts
+++ JvmConcurrencyFrontend/src/domains/scenarios.ts
@@ -1,29 +1,29 @@
-import scenario1 from "@assets/scenarios/1/scenario-1.txt";
-import scenario2 from "@assets/scenarios/2/scenario-2.txt";
-import scenario3 from "@assets/scenarios/3/scenario-3.txt";
-import scenario4 from "@assets/scenarios/4/scenario-4.txt";
-import scenario5 from "@assets/scenarios/5/scenario-5.txt";
-import scenario6 from "@assets/scenarios/6/scenario-6.txt";
-import scenario7 from "@assets/scenarios/7/scenario-7.txt";
-import scenario8 from "@assets/scenarios/8/scenario-8.txt";
-import scenario9 from "@assets/scenarios/9/scenario-9.txt";
-import scenario10 from "@assets/scenarios/10/scenario-10.txt";
-import scenario11 from "@assets/scenarios/11/scenario-11.txt";
-import scenario12 from "@assets/scenarios/12/scenario-12.txt";
-import scenario13 from "@assets/scenarios/13/scenario-13.txt";
-import scenario1Markdown from "@assets/scenarios/1/scenario-1-md.txt";
-import scenario2Markdown from "@assets/scenarios/2/scenario-2-md.txt";
-import scenario3Markdown from "@assets/scenarios/3/scenario-3-md.txt";
-import scenario4Markdown from "@assets/scenarios/4/scenario-4-md.txt";
-import scenario5Markdown from "@assets/scenarios/5/scenario-5-md.txt";
-import scenario6Markdown from "@assets/scenarios/6/scenario-6-md.txt";
-import scenario7Markdown from "@assets/scenarios/7/scenario-7-md.txt";
-import scenario8Markdown from "@assets/scenarios/8/scenario-8-md.txt";
-import scenario9Markdown from "@assets/scenarios/9/scenario-9-md.txt";
-import scenario10Markdown from "@assets/scenarios/10/scenario-10-md.txt";
-import scenario11Markdown from "@assets/scenarios/11/scenario-11-md.txt";
-import scenario12Markdown from "@assets/scenarios/12/scenario-12-md.txt";
-import scenario13Markdown from "@assets/scenarios/13/scenario-13-md.txt";
+import scenarioIoOperations from "@assets/scenarios/io-operations/scenario-io-operations.txt";
+import scenarioThreads from "@assets/scenarios/threads/scenario-threads.txt";
+import scenarioUnboundThreadPool from "@assets/scenarios/unbound-thread-pool/scenario-unbound-thread-pool.txt";
+import scenarioFixedThreadPool from "@assets/scenarios/fixed-thread-pool/scenario-fixed-thread-pool.txt";
+import scenarioVirtualThreadPool from "@assets/scenarios/virtual-thread-pool/scenario-virtual-thread-pool.txt";
+import scenarioCoroutinesBlocking from "@assets/scenarios/coroutines-blocking/scenario-coroutines-blocking.txt";
+import scenarioCoroutinesNonBlocking from "@assets/scenarios/coroutines-non-blocking/scenario-coroutines-non-blocking.txt";
+import scenarioKtor from "@assets/scenarios/ktor/scenario-ktor.txt";
+import scenarioRequestThreadPool from "@assets/scenarios/request-thread-pool/scenario-request-thread-pool.txt";
+import scenarioSpringBootVirtualThreads from "@assets/scenarios/spring-boot-virtual-threads/scenario-spring-boot-virtual-threads.txt";
+import scenarioVirtualThreadsAndCoroutines from "@assets/scenarios/virtual-threads-and-coroutines/scenario-virtual-threads-and-coroutines.txt";
+import scenarioVirtualThreadsCancellation from "@assets/scenarios/virtual-threads-cancellation/scenario-virtual-threads-cancellation.txt";
+import scenarioCoroutinesCancellation from "@assets/scenarios/coroutines-cancellation/scenario-coroutines-cancellation.txt";
+import scenarioIoOperationsMarkdown from "@assets/scenarios/io-operations/scenario-io-operations-md.txt";
+import scenarioThreadsMarkdown from "@assets/scenarios/threads/scenario-threads-md.txt";
+import scenarioUnboundThreadPoolMarkdown from "@assets/scenarios/unbound-thread-pool/scenario-unbound-thread-pool-md.txt";
+import scenarioFixedThreadPoolMarkdown from "@assets/scenarios/fixed-thread-pool/scenario-fixed-thread-pool-md.txt";
+import scenarioVirtualThreadPoolMarkdown from "@assets/scenarios/virtual-thread-pool/scenario-virtual-thread-pool-md.txt";
+import scenarioCoroutinesBlockingMarkdown from "@assets/scenarios/coroutines-blocking/scenario-coroutines-blocking-md.txt";
+import scenarioCoroutinesNonBlockingMarkdown from "@assets/scenarios/coroutines-non-blocking/scenario-coroutines-non-blocking-md.txt";
+import scenarioKtorMarkdown from "@assets/scenarios/ktor/scenario-ktor-md.txt";
+import scenarioRequestThreadPoolMarkdown from "@assets/scenarios/request-thread-pool/scenario-request-thread-pool-md.txt";
+import scenarioSpringBootVirtualThreadsMarkdown from "@assets/scenarios/spring-boot-virtual-threads/scenario-spring-boot-virtual-threads-md.txt";
+import scenarioVirtualThreadsAndCoroutinesMarkdown from "@assets/scenarios/virtual-threads-and-coroutines/scenario-virtual-threads-and-coroutines-md.txt";
+import scenarioVirtualThreadsCancellationMarkdown from "@assets/scenarios/virtual-threads-cancellation/scenario-virtual-threads-cancellation-md.txt";
+import scenarioCoroutinesCancellationMarkdown from "@assets/scenarios/coroutines-cancellation/scenario-coroutines-cancellation-md.txt";
 // import scenarioTesting from "@assets/scenarios/scenario-testing.txt";
 // import scenarioTestingMarkdown from "@assets/scenarios/scenario-testing-md.txt";
 
@@ -44,8 +44,8 @@ const scenarios: ScenarioType[] = [
       "client - java http client blocking",
       "network call in series",
     ],
-    file: scenario1,
-    markdown: scenario1Markdown,
+    file: scenarioIoOperations,
+    markdown: scenarioIoOperationsMarkdown,
   },
   {
     id: "threads",
@@ -55,8 +55,8 @@ const scenarios: ScenarioType[] = [
       "client - java http client blocking",
       "network call in parallel with thread pool",
     ],
-    file: scenario2,
-    markdown: scenario2Markdown,
+    file: scenarioThreads,
+    markdown: scenarioThreadsMarkdown,
   },
   {
     id: "unbound-thread-pool",
@@ -67,8 +67,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "unbound thread pool",
     ],
-    file: scenario3,
-    markdown: scenario3Markdown,
+    file: scenarioUnboundThreadPool,
+    markdown: scenarioUnboundThreadPoolMarkdown,
   },
   {
     id: "fixed-thread-pool",
@@ -79,8 +79,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "fixed thread pool",
     ],
-    file: scenario4,
-    markdown: scenario4Markdown,
+    file: scenarioFixedThreadPool,
+    markdown: scenarioFixedThreadPoolMarkdown,
   },
   {
     id: "virtual-thread-pool",
@@ -91,8 +91,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "virtual thread pool",
     ],
-    file: scenario5,
-    markdown: scenario5Markdown,
+    file: scenarioVirtualThreadPool,
+    markdown: scenarioVirtualThreadPoolMarkdown,
   },
   {
     id: "coroutines-blocking",
@@ -103,8 +103,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "coroutines",
     ],
-    file: scenario6,
-    markdown: scenario6Markdown,
+    file: scenarioCoroutinesBlocking,
+    markdown: scenarioCoroutinesBlockingMarkdown,
   },
   {
     id: "coroutines-non-blocking",
@@ -115,8 +115,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "coroutines",
     ],
-    file: scenario7,
-    markdown: scenario7Markdown,
+    file: scenarioCoroutinesNonBlocking,
+    markdown: scenarioCoroutinesNonBlockingMarkdown,
   },
   {
     id: "ktor",
@@ -127,8 +127,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "coroutines",
     ],
-    file: scenario8,
-    markdown: scenario8Markdown,
+    file: scenarioKtor,
+    markdown: scenarioKtorMarkdown,
   },
   {
     id: "request-thread-pool",
@@ -139,8 +139,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "virtual thread pool",
     ],
-    file: scenario9,
-    markdown: scenario9Markdown,
+    file: scenarioRequestThreadPool,
+    markdown: scenarioRequestThreadPoolMarkdown,
   },
   {
     id: "spring-boot-virtual-threads",
@@ -151,8 +151,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "virtual thread pool",
     ],
-    file: scenario10,
-    markdown: scenario10Markdown,
+    file: scenarioSpringBootVirtualThreads,
+    markdown: scenarioSpringBootVirtualThreadsMarkdown,
   },
   {
     id: "virtual-threads-and-coroutines",
@@ -163,8 +163,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "virtual thread pool dispatcher for coroutines",
     ],
-    file: scenario11,
-    markdown: scenario11Markdown,
+    file: scenarioVirtualThreadsAndCoroutines,
+    markdown: scenarioVirtualThreadsAndCoroutinesMarkdown,
   },
   {
     id: "virtual-threads-cancellation",
@@ -175,8 +175,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "virtual thread pool cancelations",
     ],
-    file: scenario12,
-    markdown: scenario12Markdown,
+    file: scenarioVirtualThreadsCancellation,
+    markdown: scenarioVirtualThreadsCancellationMarkdown,
   },
   {
     id: "coroutines-cancellation",
@@ -187,8 +187,8 @@ const scenarios: ScenarioType[] = [
       "network call in parallel",
       "coroutines cancelations",
     ],
-    file: scenario13,
-    markdown: scenario13Markdown,
+    file: scenarioCoroutinesCancellation,
+    markdown: scenarioCoroutinesCancellationMarkdown,
   },
   // {
   //   id: "testing",
